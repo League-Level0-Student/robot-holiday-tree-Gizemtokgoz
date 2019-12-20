d95b1d4aa4e57963f6b1449b23196d9305b092c6
League Student <league@iMac-24.attlocal.net>
2019-12-19 18:16:17 -0800

    HolidayTree :christmas_tree:

--- winter_recipes/Tree.java
+++ winter_recipes/Tree.java
@@ -16,12 +16,11 @@ public class Tree  {
   static double scale = 1.1;   
 	
   public static void main(String[] args) {
-
+	  rob.changeRobot("https://i.pinimg.com/originals/a5/f0/a4/a5f0a48dac0f93f609ee578d7aeb7b51.png");
 	  // 1. Make the robot go fast
-
-	  
+	  rob.setSpeed(100);
 	  // 2. Tell the robot to put its pen down
-
+	  rob.penDown();
 	  // 3. Find the method called drawBranches below and complete its code
       drawBranches();
       
@@ -36,51 +35,58 @@ public class Tree  {
     
     static void drawBranches() {
         //  Change the color of the line the robot draws to green    
-
+    	rob.setPenColor(0,200,0);
         //  Make an int variable called turnAmount and set it to 175
-
+    	int turnAmount = 172;
         //  Set the robot's angle so it faces right
-
+    	rob.turn(90);
         //  Make a for loop that repeats 11 times and put the rest of the code that draws branches inside the for loop
-   
-
+    		for (int i = 0; i < 18; i++) {		
             // Move the robot the width of the tree (use the treeWidth variable from the top of this program)
-    		
+    		rob.move(treeWidth);
             // Turn the robot (use the turnAmount variable you made earlier)
-    		
+    		rob.turn(turnAmount);
             // Scale up the treeWidth (    treeWidth *= scale;   )
-    		
+    		treeWidth *= scale;
             // Move the robot the width of a tree again
-    		 
+    		rob.move(treeWidth);
             // Turn the robot by the current turn amount, but this time turn to the LEFT (hint: negative turns go left)
-    		
+    		rob.turn(-turnAmount);
             // Scale up the treeWidth again (    treeWidth *= scale;   )
-   
+    		treeWidth *= scale;
             // Decrease turnAmount by 1
- 
+    		//turnAmount -= 1;
     	// Make sure your for loop ends here!
+    }
 }
     
     static void drawTrunk() {
         //  Move the robot half the width of the tree
-    	
+    	rob.move(treeWidth/2);
 
         // Set the robot's angle so that it is pointing straight down
-  
+    	rob.setAngle(180);
 
         // Set the pen width to the tree width divided by 10
-     
+    	rob.setPenWidth(treeWidth/10);
 
         // Set the color of the robot's pen to brown
-  
+    	rob.setPenColor(185,103,1);
 
         // Move the robot a quarter the tree width
-
+    	rob.move(treeWidth/4);
     }
     
     static void drawStar() {
         // * Challenge: Write code to draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+    	rob.setPenColor(Color.yellow);
+    	rob.setX(462);
+    	rob.setY(280);
+    	for (int i = 0; i < 5; i++) {
+    		rob.move(20);
+			rob.turn(144);
+		}
+    	rob.hide();
     }
 
 
